3c08097c500ed858738c064424f6dd043f0201ad
hauschkaured <118323804+hauschkaured@users.noreply.github.com>
2024-09-20 11:43:52 -0400

    update interact

--- interact.py
+++ interact.py
@@ -1,7 +1,14 @@
 # This code is trying out blessed.readthedocs.io's terminal package.
-
-import signal
 from blessed import Terminal
 
 term = Terminal()
 
+# This line prints what we tell it to at the "home" position of the terminal.
+# It then moves the 'y' coordinate down to the middle of the screen.
+print(term.home + term.clear + term.move_y(term.height//2))
+print(term.center("Welcome to TransitFoamer!"))
+
+# This hides the cursor until you make a keypress.
+with term.cbreak(), term.hidden_cursor():
+    inp = term.inkey()
+    
